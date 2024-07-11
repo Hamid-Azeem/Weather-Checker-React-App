05612f5c3c5bcb70fb9c9324f6e02fe6b8c9cc70
Hamid-Azeem <hamidazeem6842@gmail.com>
2024-07-11 11:05:43 +0500

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -6,19 +6,20 @@ import Background from "./Assets/Background.mp4"
 function App() {
 
    return (
+      
       <div className="relative w-full h-screen overflow-hidden">
           <video  className="absolute top-0 left-0 w-full h-full object-cover -z-10" autoPlay loop muted >
             <source src={Background} type="video/mp4" />
             Your browser does not support the video tag.
+            
          </video>
          <Header />
 
          <Weather />
-         
-     
+        
       </div>
    );
-};
+}
 
 
 export default App;
